de7f87cb558ee92cd614a65dce39f49cf510a2d9
yeaminsarder <yeaminsardersr@gmail.com>
2025-08-28 03:11:14 +0600

    username check logic

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "";
+$username = "root";
+$password = ''; // todo: fetch from environment
+
+$port = 4056;
+$dbname = 'dams';
+  try {
+      //$conn = mysqli_connect("$servername:$port", $username, $password, $dbname);
+      
+      $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password, array (PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false));
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully\n";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
--- login.php
+++ login.php
@@ -1,5 +1,17 @@
 <?php
+require "connect.php";
 $uname = $_POST['username'];
 $pass  = $_POST['password'];
 echo $uname . "<br>";
-echo $pass;
+echo $pass . "<br>";
+
+$sql = "select username, sha256 from pass where username = '" . $uname . '\';';
+$r = $conn->query($sql)->fetchAll();
+if (!empty($r)) {
+foreach ($r as $row) {
+    if ($row['sha256'] == hash("sha256",$pass)){
+        echo "loged in as ".$row['username'];
+        return;
+    }
+};
+}
