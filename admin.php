26b69739cc15c604b2caf8e2c0a87fb0c99dfcca
Abir <144364412+Hafiz-110@users.noreply.github.com>
2025-09-10 21:40:24 +0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start()
 ?>
-<h1> Welcome BOSS.</h1>
+<h1> Welcome BOSS!!!</h1>
 <h2> your admin id is <?= $_SESSION['user_id'] ?>
     <p> TODO: fetch admin info with id <?= $_SESSION['user_id'] ?> <br>
         develop ui.
