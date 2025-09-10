df5c284f6dc34716df9a56665553d9eafc9ba282
yeaminsarder <yeaminsardersr@gmail.com>
2025-09-10 21:50:33 +0600

    login fix for db001

--- auth.php
+++ auth.php
@@ -3,14 +3,14 @@ session_start();
 require "connect.php";
 $uname = $_POST['username'];
 $pass  = $_POST['password'];
-$sql = "select user_id, username, sha256, idtype from pass where username = '" . $uname . '\';';
+$sql = "select user_id, id_type, pswd_hash from account where username = '" . $uname . '\';';
 $r = $conn->query($sql)->fetchAll();
 if (!empty($r)) {
 foreach ($r as $row) {
-    if ($row['sha256'] == hash("sha256",$pass)){
-        $type = $row['idtype'];
+    if ($row['pswd_hash'] == hash("sha256",$pass)){
+        $type = $row['id_type'];
         $_SESSION['user_id'] = $row['user_id'];
-        $_SESSION['idtype'] = $row['idtype'];
+        $_SESSION['idtype'] = $row['id_type'];
         $_SESSION['is_logged_in'] = 1;
         header('Location: /index.php');
         exit();
